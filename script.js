620fe40799ca42d2e13ef3bf6cb9c8f22396e11f
Anton <art-medovikov@yandex.ru>
2022-04-15 16:12:43 +0300

    feat: опрос

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-const example = '0';
+
+const  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+//  personalMovieDB[movies] = prompt('Один из последних фильмов', 'Титаник');
+
+const a = prompt('Один из последнизх фильмов', ''),
+      b = prompt('На сколько оцените его?', '' ),
+      c = prompt('Один из последнизх фильмов', ''),
+      d = prompt('На сколько оцените его?', '' );
+
+personalMovieDB.movies[a] = b;
+
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
